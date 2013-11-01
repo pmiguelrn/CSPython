c94d0ecb0d9d182b8fe13dbe8d013bf9b383c282
pmiguelrn <stuffpedro@gmail.com>
2013-11-01 11:45:06 +0000

    data structures to organize keywords and urls working

--- queryDS.py
+++ queryDS.py
@@ -16,29 +16,21 @@
 index = []
 
 def add_to_index(index,keyword,url):
-    
+    sel = 0
     for e in index:
-        if keyword != e[0]:
-            lis = [keyword,[url]]
-            index.append(lis)
         if keyword == e[0]:
+            sel = 1
             e[1].append(url)
 
-    if len(index) == 0:
+    if sel == 0:
         lis = [keyword,[url]]
         index.append(lis)
 
     return index
 
-
-
-
-
-
-
 add_to_index(index,'udacity','http://udacity.com')
-#add_to_index(index,'computing','http://acm.org')
-#add_to_index(index,'udacity','http://npr.org')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
 print index
 #>>> [['udacity', ['http://udacity.com', 'http://npr.org']], 
 #>>> ['computing', ['http://acm.org']]]
